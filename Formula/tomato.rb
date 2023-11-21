205c7585bb298b7c4bb63a8135df83ce56c62010
Faiz Mokhtar <mfmokhtar@gmail.com>
2023-11-22 00:17:41 +0800

    try to distribute it as cask

--- Formula/tomato.rb
+++ Formula/tomato.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 
-class Tomato < Formula
+cask "tomato" do
     version "1.0"
     sha256 "2b2b5199290bcc66e3033d829e406b9e324bdf111f462a038d354fc18c2d6cca"
 
