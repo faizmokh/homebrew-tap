455f3b2d74eb445cc8ca694876a73121689a9a63
Faiz Mokhtar <mfmokhtar@gmail.com>
2023-11-22 00:12:30 +0800

    update formula

--- Formula/tomato.rb
+++ Formula/tomato.rb
@@ -2,19 +2,15 @@
 #                https://rubydoc.brew.sh/Formula
 
 class Tomato < Formula
+    version "1.0"
+    sha256 "2b2b5199290bcc66e3033d829e406b9e324bdf111f462a038d354fc18c2d6cca"
+
+    url "https://github.com/faizmokh/tomato/releases/download/1.0/Tomato.app.zip"
+    name "Tomato"
     desc "a simple pomodoro timer for macos menu bar"
     homepage "https://github.com/faizmokh/tomato"
-    url "https://github.com/faizmokh/tomato/releases/download/1.0/Tomato.app.zip"
-    sha256 "2b2b5199290bcc66e3033d829e406b9e324bdf111f462a038d354fc18c2d6cca"
     license "MIT"
-    version "1.0"
     
-    def install
-        prefix.install "Tomato.app"
-    end
-  
-    test do
-      system "false"
-    end
+    app "Tomato.app"
   end
   
