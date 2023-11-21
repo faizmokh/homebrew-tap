847a2a8560cd2b0b4e809db6e2ef6ef0a8e36032
Faiz Mokhtar <mfmokhtar@gmail.com>
2023-11-21 23:51:11 +0800

    add tomato formula

---
+++ Formula/tomato.rb
@@ -0,0 +1,20 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+
+class Tomato < Formula
+    desc "a simple pomodoro timer for macos menu bar"
+    homepage "https://github.com/faizmokh/tomato"
+    url "https://github.com/faizmokh/tomato/releases/download/1.0/Tomato.app.zip"
+    sha256 "2b2b5199290bcc66e3033d829e406b9e324bdf111f462a038d354fc18c2d6cca"
+    license "MIT"
+    
+    def install
+        system "unzip", "Tomato.app.zip"
+        system "mv", "Tomato.app", "/Applications"
+    end
+  
+    test do
+      system "false"
+    end
+  end
+  
