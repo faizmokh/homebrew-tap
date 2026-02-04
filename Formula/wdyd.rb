5bca67ec09e919dab4aec87394df97780f2cdc14
Faiz Mokhtar <tools@faizmokh.com>
2026-02-04 22:48:57 +0800

    chore: update wdyd tag

--- Formula/wdyd.rb
+++ Formula/wdyd.rb
@@ -4,8 +4,7 @@
 class Wdyd < Formula
   desc "TUI application for tracking daily work plans and blockers"
   homepage "https://github.com/faizmokh/wdyd"
-  # TODO: Update to use tagged release URLs (e.g., refs/tags/v0.1.0) when releases are made
-  url "https://github.com/faizmokh/wdyd/archive/refs/heads/main.tar.gz"
+  url "https://github.com/faizmokh/wdyd/archive/refs/tags/0.1.0"
   version "0.1.0"
   sha256 "583029b8454f0448062a92ed4ca9790e995e082998c75056e83eb163eec72d45"
   license "MIT"
